41e17f1602d820ae7984c5ced852e04f8b563968
YuJin <103713433+YujinKim164@users.noreply.github.com>
2023-02-19 00:38:39 +0900

    Update myPage.js

--- myPage.js
+++ myPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getAuth } from "firebase/auth"; 
 import { collection, setDoc, doc, updateDoc, deleteField, deleteDoc, addDoc, getDoc, getDocs } from "firebase/firestore";
 import { auth, dbService, storage } from '../fbase';
 import { useState, useEffect } from "react";
 import "../common.css";
-
 
 
 function MyPage() {
@@ -12,7 +12,7 @@ function MyPage() {
   const [modify, setModify] = useState(true);
   const [uid, setUid] = useState("");
   const [userEmail, setuserEmail] = useState("");
-  const [currentName, setCurrentName] = useState("");
+  const [currentName , setCurrentName] = useState("");
   const [datam1, setDatam1] = useState("");
   const [datam2, setDatam2] = useState("");
   const [datam3, setDatam3] = useState("");
@@ -37,22 +37,18 @@ function MyPage() {
   const [makecrew2, setMakecrew2] = useState();
   const [makecrew1, setMakecrew1] = useState();
 
-
-  // --------------------------------------------------------- 여기부터 새로 짜는 코드 ---------------------------------------------------------------------------------------------- //    
-
+  
   const [nickname, setNickname] = useState(""); // 닉네임 재설정 부분 초기화 
-
   finduid();
   setUserInfo();
   readM(); // 찾은 uid로 M1 정보를 읽어오는 함수 
   showM1();
   showM2();
-
-
+  
   useEffect(() => {     
-    console.log(auth.currentUser);                      // 한번만 닉네임 유저 이름으로 설정 
-    // setNickname(auth.currentUser.displayName);
-    // setCurrentName(auth.currentUser.displayName);
+  console.log(auth.currentUser);                      // 한번만 닉네임 유저 이름으로 설정 
+  // setNickname(auth.currentUser.displayName);
+  // setCurrentName(auth.currentUser.displayName);
   }, [])
 
   function changename() {
@@ -78,10 +74,16 @@ function MyPage() {
     setModify(!instance);
   }
 
-
-
-  
-
+  async function finduid() { // firebase Update : 함수 원하는 collection 안에 원하는 doc 안에 내용을 읽어올 때 사용한다.
+    const docRef = doc(dbService, "currentuser", "now");
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const data = docSnap.data();
+      const instance = data.uid;
+      setUid(instance);
+      console.log(uid);
+    }
+  }
   async function setUserInfo() { // firebase Update : 함수 원하는 collection 안에 원하는 doc 안에 내용을 읽어올 때 사용한다.
     const docRef = doc(dbService, "user", uid);
     const docSnap = await getDoc(docRef);
@@ -93,37 +95,22 @@ function MyPage() {
     } 
   }
 
- 
-  async function finduid() { // firebase Update : 함수 원하는 collection 안에 원하는 doc 안에 내용을 읽어올 때 사용한다.
-    const docRef = doc(dbService, "currentuser", "now");
+  async function showM1() { // firebase Update : 함수 원하는 collection 안에 원하는 doc 안에 내용을 읽어올 때 사용한다.
+    const docRef = doc(dbService, "crew", mdata);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
-      const data = docSnap.data();
-      const instance = data.uid;
-      setUid(instance);
-      console.log(uid);
+      console.log("Document data:", docSnap.data());
+      const a = docSnap.data()['CrewTitle'];
+      const b = docSnap.data()['Intro']
+     
+      setmc2T(a);
+      setmc2I(b);
+      console.log(makecrew1);
+    } else {
+      console.log("No such document!");
+      
     }
   }
-
-  
-
-  async function showM1() { // firebase Update : 함수 원하는 collection 안에 원하는 doc 안에 내용을 읽어올 때 사용한다.
-      const docRef = doc(dbService, "crew", mdata);
-      const docSnap = await getDoc(docRef);
-      if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data());
-        const a = docSnap.data()['CrewTitle'];
-        const b = docSnap.data()['Intro']
-       
-        setmc1T(a);
-        setmc1I(b);
-        
-        console.log(makecrew1);
-      } else {
-        console.log("No such document!");
-        
-      }
-    }
 
   async function showM2() { // firebase Update : 함수 원하는 collection 안에 원하는 doc 안에 내용을 읽어올 때 사용한다.
     const docRef = doc(dbService, "crew", mdata2);
@@ -132,9 +119,9 @@ function MyPage() {
       console.log("Document data:", docSnap.data());
         const a = docSnap.data()['CrewTitle'];
         const b = docSnap.data()['Intro']
-       
-        setmc2T(a);
-        setmc2I(b);
+        setmc1T(a);
+        setmc1I(b);
+        
         
     } else {
       console.log("No such document!");
@@ -161,27 +148,107 @@ function MyPage() {
     }
   }
 
- 
 
-  async function readm2(props) { // firebase Update : 함수 원하는 collection 안에 원하는 doc 안에 내용을 읽어올 때 사용한다.
-    const docRef = doc(dbService, "crew", mdata[props]);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-      // setFirstStep(docSnap.data().create);
-      const a = docSnap.data();
-      setDatam2(a);
-      
-    } else {
-      console.log("No such document!");
-      
-    }
+
+async function readm2(props) { // firebase Update : 함수 원하는 collection 안에 원하는 doc 안에 내용을 읽어올 때 사용한다.
+  const docRef = doc(dbService, "crew", mdata[props]);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+    // setFirstStep(docSnap.data().create);
+    const a = docSnap.data();
+    setDatam2(a);
+    
+  } else {
+    console.log("No such document!");
+    
   }
+}
 
- 
+
 
   
+  // async function updateNameData() { // firebase Update : 함수 원하는 collection 안에 원하는 doc 안에 내용을 읽어올 때 사용한다.
+  //   const docRef = doc(dbService, "userboard", emailName);
+  //   const docSnap = await getDoc(docRef);
+  //   if (docSnap.exists()) {
+  //     console.log("Document data:", docSnap.data());
+  //     const user = docSnap.data();
+  //     // setNewNickName(docSnap.data['NickName']);
+  //     setNewNickName(user['NickName']);
+  //   } else {
+  //     console.log("No such document!");
+  //   }
+  // }
 
+  // async function updateCrewData() { // firebase Update : 함수 원하는 collection 안에 원하는 doc 안에 내용을 읽어올 때 사용한다.
+  //   const docRef = doc(dbService, "userboard", emailName);
+  //   const docSnap = await getDoc(docRef);
+  //   if (docSnap.exists()) {
+  //     console.log("Document data:", docSnap.data());
+  //     const crew = docSnap.data();
+    
+  //     setCrewTitle(crew['EnterCrewTitle']);
+  //     setCrew(crew['EnterCrew']);
+  //   } else {
+  //     console.log("No such document!");
+  //   }
+  // }
+  
+ 
+
+  // function handleOnUpdate() {  // firebase Update : 함수 원하는 collection 안에 원하는 doc 안에 특정 field를 업데이트해주고 싶을 때 사용한다
+  //   console.log('update 시작');
+  //   const docRef = doc(dbService, "userboard", emailName);
+  //   updateDoc(docRef, {
+  //     NickName: currentName
+  //   });
+  //   if (docRef) {
+  //     console.log('update 성공');
+  //   }
+  // }
+
+  // function changeModify(props){
+  //   if(modify){
+      
+  //   console.log("함수 실행");
+  //   }else{
+  //     setNewNickName(currentName);
+  //     console.log(newNickName);
+  //     handleOnUpdate();
+  //   }
+  //   console.log("함수 실행");
+  //     const mo = !modify;
+  //   setModify(mo);
+  // }
+
+  
+  // function handleOnUpdate() {  
+  //   console.log('update 시작');
+  //   const docRef = doc(dbService, "crewPost");
+  //   updateDoc(docRef, {
+  //     CrewTitle: CrewTitle,
+  //     Oneday: oneday,
+  //     Category: Category,
+  //     Intro: Intro,
+  //   });
+  //   if (docRef) {
+  //     setCrew();
+  //     console.log('update 성공');
+  //   }
+  // }
+  // //Delete
+  // function handleOnDelte() { 
+  //   console.log('delete 시작');
+  //   const docRef = doc(dbService, "crewBoard", 'crewInformation');
+  //   deleteDoc(docRef, {
+  //     delete: deleteField()
+  //   });
+  //   if (docRef) {
+  //     setCrew();
+  //     console.log('delete 성공');
+  //   }
+  // }
 
   const onChange = (event) => { //input 값이 입력 될 때 onchange를 통해 자동적으로 setState해준다! = 동기화 시켜주기
     const {
@@ -190,15 +257,18 @@ function MyPage() {
     console.log(value);
     setCurrentName(value);
   };
-
+  
   return (
     <body>
-      {/* <header>
+        <header>
               <div class="site-title">
-                <img src="/img/로고.png" alt="logo" class="logoBox"/>
-                <img src="/img/crewSearchBt.png" alt="searchBt" class="searchBox" />
+              <Link to="/"><img src="/img/로고.png" alt="logo" class="logoBox"/></Link>
+              <Link to="/findCrew"><img src="/img/crewSearchBt.png" alt="searchBt" class="searchBox" /></Link>
+              <p class="headerSection"/>
+              <p class="nameSection">{nickname}</p>
+              <img src="./img/google.jpg" class="profileIcon" />
               </div>
-              
+
             </header>     
             <ul>
             <li><a href="/">홈</a></li>
@@ -209,10 +279,11 @@ function MyPage() {
             <a href="https://pardhgu.notion.site/PARD-cc679bfd495b4074b9142abfa3db7046"><div class="navBarIntro">PARD 소개</div></a>
         <img src="./img/copyright.png" alt="" class="copyright"/>
         <div class="navBarIntro2">2023 크루의 숲</div>
-
-        <div><p1>안녕</p1></div>
-    
-        <div>
+        <p class="myProfile">내 프로필</p>
+        <div class="myProfileBox">
+          
+        </div>
+        {/* <div>
             <p class="findCrewText">크루 찾기</p>
             <div class="findCrewBox">
                 <img src="/img/카테고리.png" alt="" class="categoryLo"/>
@@ -224,59 +295,56 @@ function MyPage() {
                 크루 소개: 저희 ‘크루어스'는 테니스 크루로, 생긴지 2년정도 됐습니다! 테니스 궁금하신 분들 함께해요!<br></br>
                 크루 정모 일정 : 2023.02.10(금) 서울 종로구 테니스장 예정<br></br> 
                 크루 활동 지역 : 서울 전지역<br></br>
-
-  
                 </p>
-                <img src="/img/수정.png" alt="" class="applyBt2" onClick={handleOnUpdate}/>
-                <img src="/img/삭제.png" alt="" class="applyBt" onClick={handleOnDelte}/>
+                {/* <img src="/img/수정.png" alt="" class="applyBt2" onClick={handleOnUpdate}/>
+                <img src="/img/삭제.png" alt="" class="applyBt" onClick={handleOnDelte}/> 
                 
                 
             </div>
+        </div>
+  */}
 
-        <h1>findCrew</h1>
-        <p>findCrew page</p>
-        </div> */}
-
-
-
-      <h1>내 프로필 </h1>
-      
+      <div class="testProfile">      
       {modify
         ? <div>
-          <p><img src="./img/google.jpg" /> </p><p1>{nickname} <button onClick={changeMyinfo} title='modifybtn'  >수정</button></p1><br></br><p1>{userEmail}</p1><br></br>
-          <p>닉네임 :    {nickname} </p> <br></br>
-          <p>소셜 로그인 :   Google</p> <br></br>
-          <p>이메일 :   {userEmail} </p>
-          <p1>내가 만들은 크루 내용</p1> <br></br>
-          <p1>{mc1T}</p1> <br></br>
-          <p1>{mc1I}</p1> <br></br><br></br>
-          <p1>두번째 내가 만든 크루 내용</p1>
-          <br></br>
-          <p1>{mc2T}</p1> <br></br>
-          <p1>{mc2I}</p1>
-
-
-        </div>
-        : <div>
-
-          <p1>닉네임 :    {currentName} <button onClick={changeMyinfo} title='modifybtn'  >저장</button></p1> <br></br>
-          <br></br>
-          <input type='text' value={currentName} placeholder={nickname  } reqiured onChange={onChange} />
+          <img src="./img/google.jpg" class="profilePic" />
+          <p1 class="nameText">{nickname}</p1>   
+          <img src="/img/수정.png" class="editBt" onClick={changeMyinfo}/>
+          <p1 class="emailText">{userEmail}</p1>
+          <div class="profileDiv"/>
+            <p class="nicknameText">닉네임</p>
+            <p class="nicknameT">{nickname} </p> 
+            <p class="loginRoute">소셜 로그인</p> 
+            <p class="googleLogin">Google</p>
+            <p class="addressText">이메일</p>
+            <p class="address">{userEmail}</p>
           
         </div>
-      }
-
-
-
+        : <div>
+          <img src="./img/google.jpg" class="profilePic" />
+          <p1 class="nameText">{nickname}</p1>   
+          <img src="/img/저장.png" class="editBt" onClick={changeMyinfo}/>
+          <p1 class="emailText">{userEmail}</p1>
+          <div class="profileDiv"/>
+          <p class="nicknameText">닉네임</p>
+          <input type='text' value={currentName} placeholder={nickname} class="editNicknameBox" required onChange={onChange} />
+        </div>
+        }
+      </div>
+      <p class="myBoard">✍🏻 내가 쓴 글</p>
+      <p1 class="myBoardTitle1">{mc1T}</p1>
+      <p1 class="myBoardIntro1">{mc1I}</p1>
+      <p1 class="myBoardTitle2">{mc2T}</p1>
+      <p1 class="myBoardIntro2">{mc2I}</p1>
+      <p class="myCrew">👥 내 크루 (내가 신청한 크루)</p>
+      <p class="myInterest">💚 내가 관심있는 크루</p>
       {/* <h1> 내 크루 </h1> <br></br>
       <h1><img src="./img/samson.png" /> {crewTitle} <br></br> {crew} </h1> */}
-
-
+     
+    
     </body>
-
+    
   );
 }
-
-
 
 export default MyPage;
